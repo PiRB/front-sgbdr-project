81d686a2918cdd4ede048e72d8d75d2a8e1506f7
PiRB <pierrebrivio14@gmail.com>
2022-02-12 22:09:09 +0100

    change front

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
     <div class="home__table-container">
-      <table class="table is-fullwidth has-background-dark is-narrow is-hoverable">
+      <table class="table is-fullwidth is-hoverable is-narrow is-striped">
         <thead>
           <tr>
             <th
               v-for="category in categories"
               :key="category.id"
               @click="updateFilters(category.id)"
-              class="has-text-white item-hover"
+              class="item-hover has-text-primary"
             >
               {{ category.title }}
             </th>
@@ -16,7 +16,7 @@
               <div :class="dropdownClasses" class="dropdown" @click="openDropdown">
                 <div class="dropdown-trigger">
                   <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                    <span>Elements affichés: {{ limit === 0 ? 'Tous les films' : limit }}</span>
+                    <span>Number of films: {{ limit === 0 ? 'Tous les films' : limit }}</span>
                     <span class="icon is-small">
                       <i class="fas fa-angle-down" aria-hidden="true"></i>
                     </span>
@@ -42,7 +42,7 @@
           <tr
             v-for="film in films"
             :key="film.title"
-            class="has-text-white"
+            class=""
           >
             <td>{{ film.title }}</td>
             <td>{{ film.price || 'Prix non renseigné' }}</td>
@@ -87,18 +87,18 @@ export default {
   data() {
     return {
       categories: [
-        { id: 1, title: 'Nom' },
-        { id: 2, title: 'Prix' },
-        { id: 3, title: 'Classement' },
-        { id: 4, title: 'Genre' },
-        { id: 5, title: 'Nombre de locations' },
+        { id: 1, title: 'Title' },
+        { id: 2, title: 'Price' },
+        { id: 3, title: 'Rating' },
+        { id: 4, title: 'Category' },
+        { id: 5, title: 'Number of Rates' },
       ],
       limitList: [
         { value: 10, title: '10' },
         { value: 25, title: '25' },
         { value: 50, title: '50' },
         { value: 100, title: '100' },
-        { value: 0, title: 'Tout afficher' },
+        { value: 0, title: 'Display all' },
       ],
       films: [],
       nbPages: 0,
@@ -141,7 +141,7 @@ export default {
       const { limit, page, orderType, orderByDesc } = this;
       this.isLoading = true;
       axios
-        .get(`https://film-api-lgbdr.herokuapp.com/api/film?limit=${limit}&page=${page}&orderType=${orderType}&orderByDesc=${orderByDesc}`)
+        .get(`https://api-sgbdr-project.herokuapp.com/api/films?limit=${limit}&page=${page}&orderType=${orderType}&orderByDesc=${orderByDesc}`)
         .then(({ data }) => {
           this.films = data.films;
           this.nbPages = data.nbPages;
@@ -188,7 +188,6 @@ export default {
 
         th {
           position: sticky;
-          background-color: rgb(41, 44, 46);
           top: 0
         }
       }
@@ -198,9 +197,8 @@ export default {
 
       &:hover {
         cursor: pointer;
-        transform: scale(1.02);
-        background-color: rgb(29, 31, 32);
-        border-radius: 5px;
+        background-color: rgb(219, 219, 219);
+        color: hsl(217, 71%, 53%) !important;
       }
     }
 
@@ -214,16 +212,6 @@ export default {
       }
     }
 
-    &__loader {
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      top: 0;
-      left: 0;
-      background-color: rgba(14, 13, 13, 0.74);
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
+   
   }
 </style>
